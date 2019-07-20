302d9c9639860fd3ec238851a1173dcfb8191ce2
Marco De La Pierre <marco.delapierre@gmail.com>
2019-07-20 22:39:52 +0800

    longq in job1 script for zeus

--- extra/job1.sh
+++ extra/job1.sh
@@ -2,8 +2,8 @@
 
 #SBATCH --job-name=Nextflow-master-nanopore
 #SBATCH --account=pawsey0001
-#SBATCH --partition=workq
-#SBATCH --time=1-00:00:00
+#SBATCH --partition=longq
+#SBATCH --time=4-00:00:00
 #SBATCH --no-requeue
 #SBATCH --export=none
 
