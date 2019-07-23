f4b64c1fdb43f3c85df3a69dfc787ca77d26812f
Marco De La Pierre <marco.delapierre@gmail.com>
2019-07-23 09:37:53 +0800

    trace and report in job scripts and config

--- extra/job2.sh
+++ extra/job2.sh
@@ -11,4 +11,5 @@ module load nextflow
 
 nextflow run marcodelapierre/nanopore-nf  -resume \
   -profile zeus --slurm_account='pawsey0281' -name nxf-${SLURM_JOB_ID} \
+  -with-trace trace-${SLURM_JOB_ID}.txt -with-report report-${SLURM_JOB_ID}.html \
   --read_dir='sample6' --seqid='D10930.1,MK116873.1'
