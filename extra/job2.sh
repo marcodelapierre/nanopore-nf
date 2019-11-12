19ebd471199c08b6d41c84ec16bf22c5219e335f
Marco De La Pierre <marco.delapierre@gmail.com>
2019-11-12 21:23:02 +0800

    resume in config

--- extra/job2.sh
+++ extra/job2.sh
@@ -10,7 +10,7 @@
 module load singularity/3.3.0
 module load nextflow
 
-nextflow run marcodelapierre/nanopore-nf  -resume \
+nextflow run marcodelapierre/nanopore-nf \
   -profile zeus --slurm_account='pawsey0281' -name nxf-${SLURM_JOB_ID} \
   -with-trace trace-${SLURM_JOB_ID}.txt -with-report report-${SLURM_JOB_ID}.html \
   --read_dir='sample6' --seqid='D10930.1,MK116873.1'
